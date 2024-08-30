e843f36cb7a7848596c48d962670676e61fd1b8c
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-08-30 13:15:17 -0300

    feat: adicionar contador para saber quantos clientes temos (ID)

--- src/main/java/org/example/project/Person.java
+++ src/main/java/org/example/project/Person.java
@@ -2,6 +2,9 @@ package org.example.project;
 
 public class Person {
 
+    // Contar quantidade de clientes (ID)
+    private static int counter = 1;
+
     private String name;
     private String cpf;
     private String email;
