7f10152231cc1eec38a23025e731ce950a0557c1
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-08-29 21:58:16 -0300

    feat: Verificar se o CPF tem 11 dígitos

--- src/main/java/org/example/project/utils/CPFValidator.java
+++ src/main/java/org/example/project/utils/CPFValidator.java
@@ -6,6 +6,11 @@ public class CPFValidator {
         // Remover caracteres não numéricos
         cpf = cpf.replaceAll("\\D", "");
 
+        // Verificar se o CPF tem 11 dígitos
+        if (cpf.length() != 11){
+            return false; // CPF inválido se não tiver 11 dígitos
+        }
+
         return false;
     }
 }
